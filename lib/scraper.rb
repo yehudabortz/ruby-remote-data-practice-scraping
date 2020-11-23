ac1776599488ba87f135f3439fa77433e524a6a4
Yehuda Bortz <yehudabortz@icloud.com>
2020-11-23 12:03:34 -0600

    commit tree fa31df00da62ee1a37e3560661892c0ae58ded1c

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,10 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
+doc = Nokogiri::HTML(html)
 
+courses = doc.css(".title-oE5vT4")
 
+courses.each do |course|
+    puts course.text.strip
+    end
